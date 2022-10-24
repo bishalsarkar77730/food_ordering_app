ff020039fdc5aaf6cfc025f9f939a545df6aaefe
Bishal Sarkar <69084883+bishalsarkar77730@users.noreply.github.com>
2022-10-24 13:10:03 +0530

    Backend Completed

--- dist/config/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.APP_SECRET = exports.Mongo_URI = void 0;
-exports.Mongo_URI = "mongodb+srv://Bishal77730:Bishal77730@cluster0.20ptmhg.mongodb.net/?retryWrites=true&w=majority";
-exports.APP_SECRET = "naidunganaikarunga";
-//# sourceMappingURL=index.js.map
